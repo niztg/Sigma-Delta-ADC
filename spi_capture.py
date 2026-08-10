013f556402d9b8157893288cc15785378ec8d659
Niz Ansari <nizansari@macbookair.lan>
2026-08-10 17:27:23 -0400

    Added capture bitstream function

--- spi_capture.py
+++ spi_capture.py
@@ -16,3 +16,12 @@ def open_spi(bus, device, clock_hz):
     spi.bits_per_word = 8
     spi.no_cs = False
     return spi
+
+def capture_bitstream(spi, duration_s, bytes_per_xfer):
+    raw_bytes = []
+    dummy = [0x00] * bytes_per_xfer # zeroes to send out on the (unused) MOSI pin
+    t_start = time.monotonic()
+    while time.monotonic() - t_start < duration_s: # loops until one second has elapsed
+        raw_bytes.extend(spi.xfer2(dummy)) # queues the last 8 bytes from the MISO pin
+    return np.array(raw_bytes, dtype=np.uint8)
+
